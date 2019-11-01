eb8072c4441cdf8c6420ac5bc4da5beaac333bcd
escc1122 <escc1122@gmail.com>
2019-11-01 16:37:34 +0800

    Update User.java

--- cloud_run/swaggertest/src/main/java/com/example/demo/resources/User.java
+++ cloud_run/swaggertest/src/main/java/com/example/demo/resources/User.java
@@ -4,24 +4,34 @@ import com.example.demo.data.UserData;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * Created by alma on 2019/11/1.
  */
 @RestController
 @RequestMapping("/user")
 public class User {
+  private static Map<Integer,UserData> userDataMap = new ConcurrentHashMap<>();
   @PostMapping("/add")
   public boolean addUser(@RequestBody UserData user) {
-    return false;
+    boolean r = false;
+    if (!userDataMap.containsKey(user.getId())){
+      userDataMap.put(user.getId(),user);
+      r = true;
+    }
+    return r;
   }
   @GetMapping("/find/{id}")
   @ApiOperation(notes = "測試用", value = "get user", nickname = "findById",
           tags = {"user"} )
   public UserData findById(@PathVariable("id") int id) {
+    UserData r = null;
     System.out.print(id);
-    UserData r = new UserData();
-    r.setId(id);
-    r.setName("test");
+    if (userDataMap.containsKey(id)){
+      r = userDataMap.get(id);
+    }
     return r;
   }
   @PutMapping("/update")
@@ -30,6 +40,11 @@ public class User {
   }
   @DeleteMapping("/delete/{id}")
   public boolean delete(@PathVariable("id") int id) {
-    return true;
+    boolean r = false;
+    if (userDataMap.containsKey(id)){
+      userDataMap.remove(id);
+      r=true;
+    }
+    return r;
   }
 }
